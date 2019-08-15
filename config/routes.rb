006e21bddaccef7fe0a4f25c3bef08324e5e158d
Annika Lang <Lang-annika@t-online.de>
2019-08-15 21:47:47 +0200

    MVC logic added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/cocktails', to: 'cocktails#index'
+  # get 'cocktails/new', to: 'cocktails#new'
+  # get 'cocktails/:id', to: 'cocktails#show'
+  # post 'cocktails/create', to: 'cocktails#create'
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Cocktail.create(name: "Caipirinha")
+Cocktail.create(name: "White Russian")
+Dose.create(cocktail_id: 1, ingredient_id: 1, description: "100ml")
+Dose.create(cocktail_id: 2, ingredient_id: 1, description: "100ml")
+Dose.create(cocktail_id: 1, ingredient_id: 2, description: "100ml")
+Dose.create(cocktail_id: 2, ingredient_id: 2, description: "100ml")
